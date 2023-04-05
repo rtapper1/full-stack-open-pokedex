34e0563b54b4eca93706af7439c7eb322d452281
Riku Tapper <riku.tapper@gmail.com>
2023-04-05 21:16:46 +0300

    e2e test added

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('should be able to find and navigate to bulbasaur', function() {
+    cy.visit('http://localhost:5001')
+    cy.contains('bulbasaur').click()
+    cy.contains('bulbasaur')
+    cy.contains('chlorophyll')
+  })
 })
